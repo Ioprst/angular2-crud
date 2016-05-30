a6a3e78ce5737e71aa7dd9d5c496e7bae8727cef
= <nimbew@yandex.ru>
2016-05-30 22:54:12 +0500

    essence.component - localstorage include

--- app/essence/essence.component.ts
+++ app/essence/essence.component.ts
@@ -1,21 +1,38 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {BsTableComponent} from '../components/bstable/bstable.component';
+import {LocalStorage} from '../service/storage.service';
 
 @Component({
     selector: 'essence',
     templateUrl: '/app/template/essence.component.html',
-    directives: [BsTableComponent]
+    directives: [BsTableComponent],
+    providers:[LocalStorage]
 })
 
-export class EssenceComponent {
-    title = "Essence";
-    columns = [
-        {
-            key: 'name',
-            label: 'Name'
-        }
-    ];
-    constructor() {
+export class EssenceComponent implements OnInit {
+    title:string;
+    columns: Array<any>;
+    rows: Array<any>;
+
+    _data: any;
+
+    constructor(private localStorage: LocalStorage) {}
+
+    ngOnInit(): void {
+        this.getData();
+    }
+
+    getData(){
+        this._data = this.localStorage.get(this.getTableName());
+        this.changeRows();
+    }
+
+    changeRows() {
+        this.rows = this._data;
+    }
+
+    getTableName(){
+        return '';
     }
 }
 
